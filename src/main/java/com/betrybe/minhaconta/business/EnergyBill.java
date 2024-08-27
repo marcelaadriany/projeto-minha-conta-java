73490e9e6c05be5c1519bee6f608110cd7895312
marcelaadriany <marcela.adriany@gmail.com>
2024-08-26 22:18:27 -0300

    requisito 3?

--- src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
+++ src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
@@ -32,7 +32,11 @@ public class EnergyBill {
    * Req. 3 – Calculates the total usage of a collection of devices.
    */
   public static int calculateTotalUsage(ElectronicDevice[] devices) {
-    return 0;
+    int totalUsage = 0;
+    for (ElectronicDevice device : devices) {
+      totalUsage += device.monthlyKwh();
+    }
+    return totalUsage;
   }
 
   /**
